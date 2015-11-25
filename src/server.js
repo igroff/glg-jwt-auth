338b59b4cef236493a813426346f65f50518c040
asegal / squince <asegal@glgroup.com , squince@glgroup.com>
2015-11-25 16:01:00 -0500

    WIP

--- src/server.js
+++ src/server.js
@@ -2,18 +2,19 @@
 
 var express = require('express),
     http = require('http'),
+    jwt = require('jsonwebtoken'),
+    winston = require('winston'),
+    email = require('./email-service'),
     epiquery = process.evn.EPIQUERY_URL,
     port = process.env.PORT,
-    jwt = require('jsonwebtoken'),
     secret = process.env.JWT_SECRET,
-    winston = require('winston');
     app = express();
 
 
-winston.setLevels winston.config.syslog.levels
-log = new (winston.Logger)(transports: [
+winston.setLevels(winston.config.syslog.levels);
+var log = new (winston.Logger)(transports: [
   new (winston.transports.Console)(level: 'info', timestamp: true)
-])
+]);
 
 
 // use Hogan for templating
@@ -22,19 +23,27 @@ app.enable('view cache');
 app.enable('trust proxy');
 app.engine('html', require('hogan-express'));
 
+
+// route "/healthy": Get the form
+app.get('/healthy', function(req, res) {
+  res.status(500);
+  res.send();
+});
+
+
 // route "/": Get the form
 app.get('/', function(req, res) {
   res.render('app', {target: req.params.target}, function(err,html){
     if (err) {
-      log.error(err));
+      log.error('Error rendering view',err));
       res.status(500);
       res.send();
     }
     else {
       res.send(html);
     }
-  });
-}
+  }
+});
 
 // route "/submit": Validate and handle form submission
 app.post('/submit', function(req, res) {
@@ -54,40 +63,51 @@ app.post('/submit', function(req, res) {
   var req = http.request( options, function(response) {
     var output_string,
         output,
-        res_body = {status:'error',message:''};
+        res_body = {status:'fail',message:'UNKNOWN_ERROR'};
     response.on('data', function(chunk) {
       output_string += chunk;
     });
     response.on('end', function() {
       try {
-        output = JSON.parse(output_string);
+        output = JSON.parse(output_string)[0];
       }
       catch (err) {
-        log.error(err);
+        log.error('Error parsing submitted form values', err);
       }
       finally {
         if (output != null and typeof output != 'undefined') {
-          // if invalid, send rejection message
-          // otherwise, set roles based on IDs returned, format and send email with jtw token in link
-          // send success message
+          if (output.error != null && typeof output.error != 'undefined' && output.error != '') {
+            // if invalid, send rejection message
+            res_body = {status:'fail', message: output.error}
+          }
+          else if (output.PERSON_ID != null && typeof output.PERSON_ID != 'undefined' && output.PERSON_ID != '')  {
+            // send success message
+            res_body = {status:'success', message: '', name: output.FIRST_NAME}
+            // set roles based on IDs returned, format and send email with jtw token in link
+            // for now, the only role we support is CM.  In the future, these will have to put in 
+            // an array and then joined.
+            // TODO: need to check for and verify existing jwt, if any.  If it's invalid, check reason. If reason begins with
+            // "jwt token expired at" then reuse the paylod for new token.  Otherwise generate new token from scratch.
+            // TODO: what do we do if there's no target?
+            var payload = {
+              role: (output.COUNCIL_MEMBER_ID != null && typeof output.COUNCIL_MEMBER_ID != undefined && output.COUNCIL_MEMBER_ID != "") ? "cm" : "",
+              id: output.PERSON_ID
+            }
+            /*
+              var token = jwt.sign(payload, secret);
+            */
+          }
         }
         res.writeHead(200, {"Content-Type": "application/json"});
         res.write(JSON.stringify(res_body));
         res.send();
       }
     });
-  });
-}
+  }
+});
 
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
   log.info('glg-jwt-auth listening at http://%s:%s', host, port);
 };
-
-
-  var payload = {id: 'testuser', role: 'tester'};
-  var token = jwt.sign(payload, secret);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(token);
-  res.end();
